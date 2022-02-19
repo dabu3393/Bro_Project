93a0341e9ae2648880d9775458a9050009a1f97e
Davis Burrill <97137252+dabu3393@users.noreply.github.com>
2022-02-19 14:59:04 -0700

    update #2

---
+++ bro/info.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = # Your Email
+EMAIL_HOST_PASSWORD = # Your Password
+EMAIL_PORT = 587
